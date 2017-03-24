c73dfabf8da93dccc031260daac78661d03a955a
laptop_noodle <crazynoodle@live.com>
2017-03-24 14:53:43 -0400

    add hello_cgi.py at 19:20 2017/03/26

---
+++ python-code/hello_cgi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+print "Content-type:text/html"
+print
+print '<html>'
+print '<head>'
+print '<title>Hello</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello World! This is my first CGI program</h2>'
+print '</body>'
+print '</html>'
